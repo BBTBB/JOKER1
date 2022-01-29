9f8c88aa5e3218f4e56ae1548dc79e6da6131f59
BBTBB <abassbah43@gmail.com>
2022-01-29 03:31:34 +0300

    Update all.py

--- handlers/all.py
+++ handlers/all.py
@@ -50,7 +50,7 @@ def allGP(client, message,redis):
         Bot("sendMessage",{"chat_id":chatID,"text":r.userNocc,"reply_to_message_id":message.message_id,"parse_mode":"html"})
 
     if re.search(c.sors,text):
-      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/"+BOTATE)],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/RSaiedBot")],[InlineKeyboardButton("قناة البوتات 🤖", url="t.me/BOTATE")]])
+      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/"+BBTBB)],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/FFMFF")],[InlineKeyboardButton("قناة البوتات 🤖", url="t.me/BBTBB")]])
       Botuser = client.get_me().username
       Bot("sendMessage",{"chat_id":chatID,"text":r.sors.format("@"+Botuser),"disable_web_page_preview":True,"reply_to_message_id":message.message_id,"parse_mode":"markdown","reply_markup":kb})
     
