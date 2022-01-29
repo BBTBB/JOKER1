39dbfbc9b7e6f4973b4f1e41e99ff29f31b1efef
BBTBB <abassbah43@gmail.com>
2022-01-29 03:43:52 +0300

    Update sudo.py

--- handlers/sudo.py
+++ handlers/sudo.py
@@ -282,7 +282,7 @@ def sudo(client, message,redis):
 				Bot("sendMessage",{"chat_id":chatID,"text":r.Yrp.format(tx),"reply_to_message_id":message.message_id,"parse_mode":"html"})
 			else:
 				redis.hset("{}Nbot:stepSUDO".format(BOT_ID),userID,tx)
-				kb = InlineKeyboardMarkup([[InlineKeyboardButton(r.MoreInfo, url="t.me/"+BOTATE)]])
+				kb = InlineKeyboardMarkup([[InlineKeyboardButton(r.MoreInfo, url="t.me/"+BBTBB)]])
 				Bot("sendMessage",{"chat_id":chatID,"text":r.Sendreply % tx,"reply_to_message_id":message.message_id,"parse_mode":"html","reply_markup":kb})
 			
 
@@ -360,25 +360,25 @@ def sudo(client, message,redis):
 				Files_U = ["tg.py","locks.py","rank.py","send.py"]
 				Files_B = ["bot.py","setup.py"]
 				for fnh in Files_H:
-					url = "https://raw.githubusercontent.com/VeerCli/VeerV2/master/handlers/"+fnh
+					url = "https://raw.githubusercontent.com/BBTBB/JOKER1/main/handlers/"+fnh
 					out = requests.get(url).text
 					f = open("./handlers/"+fnh,"w+")
 					f.write(out)
 					f.close()
 				for fnu in Files_U:
-					url = "https://raw.githubusercontent.com/VeerCli/VeerV2/master/utlis/"+fnu
+					url = "https://raw.githubusercontent.com/BBTBB/JOKER1/main/utlis/"+fnu
 					out = requests.get(url).text
 					f = open("./utlis/"+fnu,"w+")
 					f.write(out)
 					f.close()
 				for fnb in Files_B:
-					url = "https://raw.githubusercontent.com/VeerCli/VeerV2/master/"+fnb
+					url = "https://raw.githubusercontent.com/BBTBB/JOKER1/main/"+fnb
 					out = requests.get(url).text
 					f = open("./"+fnb,"w+")
 					f.write(out)
 					f.close()
 				for fnu in Files_L:
-					url = "https://raw.githubusercontent.com/VeerCli/VeerV2/master/lang/"+fnu
+					url = "https://raw.githubusercontent.com/BBTBB/JOKER1/main/lang/"+fnu
 					out = requests.get(url).text
 					f = open("./lang/"+fnu,"w+")
 					f.write(out)
